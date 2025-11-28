6932c364024ea11e66cd47ac772f5ec1580c9277
nely <75982461@idexperujapon.edu.pe>
2025-11-28 18:39:45 -0500

    cambios tipo_equipos.cs

--- Examen_final/Modelos/tipo_equipos.cs
+++ Examen_final/Modelos/tipo_equipos.cs
@@ -1,12 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Examen_final.Modelos
 {
     internal class tipo_equipos
     {
+        public static DataTable obtener()
+        {
+            conexion cnn = new conexion();
+            try
+            {
+                cnn.conectar();
+                string consulta = "SELECT * FROM tipo_equipos";
+                SqlCommand cmd = new SqlCommand(consulta, cnn.conectar());
+                SqlDataAdapter adaptador = new SqlDataAdapter(cmd);
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener los tipo_equipos: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                cnn.desconectar();
+            }
+
+        }
+        public static bool Crear(string nombre)
+        {
+            conexion cnn = new conexion();
+            try
+            {
+                cnn.conectar();
+                string consulta = "INSERT INTO tipo_equipos(nombre) " +
+                                  "VALUES (@nombre)";
+                SqlCommand cmd = new SqlCommand(consulta, cnn.conectar());
+                cmd.Parameters.AddWithValue("nombre", nombre);
+                int filasAfectadas = cmd.ExecuteNonQuery();
+                return filasAfectadas > 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al crear el autor: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                cnn.desconectar();
+            }
+        }
+        public static bool Editar(int tipo_equipos_id, string nombre)
+        {
+            conexion cnn = new conexion();
+            try
+            {
+                cnn.conectar();
+                string consulta = "UPDATE tipo_equipos SET nombre=@nombre WHERE tipo_equipos_id=@tipo_equpos_id";
+                SqlCommand cmd = new SqlCommand(consulta, cnn.conectar());
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@tipo_equipos_id", tipo_equipos_id);
+                int filasAfectadas = cmd.ExecuteNonQuery();
+                return filasAfectadas > 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al editar el tipo_equipos: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                cnn.desconectar();
+            }
+        }
+        public static bool Eliminar(int tipo_id)
+        {
+            conexion cnn = new conexion();
+            try
+            {
+                cnn.conectar();
+                string consulta = "DELETE FROM tipo_equipos WHERE mantenimientos_id=@mantenimientos_id";
+                SqlCommand cmd = new SqlCommand(consulta, cnn.conectar());
+                cmd.Parameters.AddWithValue("@tipo_equipos_id", tipo_id);
+                int filasAfectadas = cmd.ExecuteNonQuery();
+                return filasAfectadas > 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar tipo_eqipos " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                cnn.desconectar();
+            }
+        }
     }
 }
